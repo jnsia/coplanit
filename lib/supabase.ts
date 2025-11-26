9ccd15679191b32c8fd7aadc60e03e303d1d0a38
jnsia <kia54682@gmail.com>
2025-11-26 15:57:38 +0900

    build: eas 빌드 환경 정상화

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,9 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zuuyrkkfrnvvfvlpkyyv.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dXlya2tmcm52dmZ2bHBreXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzODA4OTMsImV4cCI6MjAzNzk1Njg5M30.75yxEQO7XVyDTiZYjOm4QnRzPaj-EDteiclbt38noHE'
-
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
